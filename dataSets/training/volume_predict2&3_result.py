1d2210611360a5db2cb9d052921b910ee6e479dc
zjw123 <lzsn@zjw123deMacBook-Pro.local>
2017-04-03 14:08:27 +0800

    run prediction

--- dataSets/training/volume_predict2&3_result.py
+++ dataSets/training/volume_predict2&3_result.py
@@ -10,5 +10,6 @@ volume_predict2 = pd.read_csv("volume_predict2_result.csv")
 volume_predict3 = pd.read_csv("volume_predict3_result.csv")
 
 volume_predict3["volume"] = 0.3 * volume_predict3["volume"] + 0.7 * volume_predict2["volume"]
+volume_predict3["volume"] = volume_predict3["volume"].apply(lambda x: round(x, 2))
 
 volume_predict3.to_csv("volume_predict2&3_result.csv")
